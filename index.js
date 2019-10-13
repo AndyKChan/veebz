60c6253ec922e0a8bfede2bb1d53c0fdc5fab936
Andy Chan <andychan94@live.com>
2019-10-12 18:43:00 -0700

    basic routing

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const app = express();
 const path = require('path')
+const http = require('http').Server(app);
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('index'))
+app.get('/makeTeams', (req, res) => res.render('makeTeams'));
+
+http.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
